67f4ba0e26218fb8b5289094e429b5badf59848f
Mahmoud Atef <mahmoud.atef3167@gmail.com>
2025-05-02 02:26:03 +0300

    feat: Add custom error, loading, and no data widgets

--- lib/src/core/widgets/core_widgets.dart
+++ lib/src/core/widgets/core_widgets.dart
@@ -1,1 +1,68 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:tarsheed/generated/l10n.dart';
+import 'package:tarsheed/src/core/utils/image_manager.dart';
 
+class CustomErrorWidget extends StatelessWidget {
+  final String message;
+  const CustomErrorWidget({super.key, required this.message});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Expanded(
+            child: Image(
+              height: 80.h,
+              width: 80.w,
+              image: AssetImage(AssetsManager.errorIcon),
+              fit: BoxFit.fill,
+            ),
+          ),
+          SizedBox(height: 20.h),
+          Text(
+            message,
+            style: TextStyle(
+                fontSize: 16.sp,
+                fontWeight: FontWeight.bold,
+                color: Colors.black),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class CustomLoadingWidget extends StatelessWidget {
+  const CustomLoadingWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(child: CircularProgressIndicator());
+  }
+}
+
+class NoDataWidget extends StatelessWidget {
+  const NoDataWidget({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Image(
+              height: 60.h,
+              width: 60.w,
+              image: AssetImage(AssetsManager.noDataFound)),
+          SizedBox(height: 30.h),
+          Text(S.of(context).noDataFound),
+        ],
+      ),
+    );
+  }
+}
